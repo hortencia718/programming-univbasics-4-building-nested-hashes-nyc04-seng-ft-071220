27b38c83242a3504e034dbdcd03964816ea4e4a7
hortencia c <hortenciaecisneros@gmail.com>
2020-07-21 18:14:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,8 +30,8 @@ def monopoly_with_third_tier
   {
     railroads: {
       pieces: 4,
-      rent_in_dollars: {one_piece_owned: 25, two_piece_owned: 50, three_piece_owned: 100, },
-      names: {}
+      rent_in_dollars: {one_piece_owned: 25, two_piece_owned: 50, three_piece_owned: 100,four_pieces_owned: 200 },
+      names:= {:reading_railroad =[],}
     }
   }
 end
