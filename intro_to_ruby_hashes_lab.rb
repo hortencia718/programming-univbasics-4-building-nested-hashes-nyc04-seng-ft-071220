94b61666c5f499a15ba9cf1eda20c7f017ab2281
hortencia c <hortenciaecisneros@gmail.com>
2020-07-21 18:34:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -55,14 +55,14 @@ def monopoly_with_fourth_tier
     railroads: {
       pieces: 4,
       rent_in_dollars: {
-        one_pieces_owned: 25, 
+        one_piece_owned: 25, 
         two_pieces_owned: 50, 
         three_piece_owned: 100,
         four_pieces_owned: 200 
       },
       names: {
-        reading_railroad: 
-        {mortgage_value: 100},
+        reading_railroad: {
+         mortgage_value: 100 },
         pennsylvania_railroad: {mortgage_value: 200},
         b_and_o_railroad: {mortgage_value: 400},
         shortline_railroa: {mortgage_value:800}
